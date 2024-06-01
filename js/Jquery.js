86ff5f9d2ce61153ec65c08073a8c8c06e73f70b
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-06-01 10:29:03 +0900

    2024-06-01

--- js/Jquery.js
+++ js/Jquery.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
     $("#hamburger-1").click(function(){
       if ($("#MAIN_NAV").css('display') == 'none') {
-        // MAIN_NAV를 flex로 설정 후 부드럽게 나타나게 합니다.
         $("#MAIN_NAV").css('display', 'flex').hide().fadeIn(700);
         $("#MAIN_Tag").fadeOut(0);
       } else {
-        // MAIN_NAV를 부드럽게 사라지게 한 후 display를 none으로 설정합니다.
         $("#MAIN_NAV").fadeOut(0, function() {
           $(this).css('display', 'none');
         });
